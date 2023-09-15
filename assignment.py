b2af2caf7058a96be7f2028ac258a4977eccd331
skater384 <113055508+skater384@users.noreply.github.com>
2023-09-15 12:41:53 -0700

    fin

--- assignment.py
+++ assignment.py
@@ -1,8 +1,40 @@
- "help", "copyright", "credits" or "license" for more information.
->>> import turtle
->>> s = turtle.getscreen()
->>> t= turtle.Turtle
->>> t.fd(200)
->>> t.forward(50)
->>> t.lt(30)
->>> t.forward(35)
+ #"help", "copyright", "credits" or "license" for more information.
+import turtle
+import time
+list1 = ("red","green","blue","yellow","purple","orange")
+s = turtle.Turtle()
+s.hideturtle()
+def key_test():
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.pensize(4)
+    t.rt(90)
+    t.color("black","skyblue")
+    t.begin_fill()
+    t.circle(60)
+    t.end_fill()
+    t.rt(90)
+    t.pensize(7)
+    t.forward(10)
+    t.pensize(4)
+    t.rt(90)
+    t.begin_fill()
+    t.circle(60)
+    t.end_fill()
+    t.penup()
+    t.goto(120,1)
+    t.pendown()
+    t.rt(45)
+    t.forward(110)
+    t.penup()
+    t.goto(-130,-5)
+    t.pendown()
+    t.forward(130)
+    
+key_test()
+for j in range(5):
+    
+    for i in list1:
+        s = turtle.bgcolor(i)
+        time.sleep(0.2)
+    
